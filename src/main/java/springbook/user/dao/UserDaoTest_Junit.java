8c4d05e417f06469c286999ceaa99935bbde4530
Lee In Seok <dlstjr9068@naver.com>
2019-03-26 21:34:51 +0900

    2-12

--- src/main/java/springbook/user/dao/UserDaoTest_Junit.java
+++ src/main/java/springbook/user/dao/UserDaoTest_Junit.java
@@ -20,22 +20,21 @@ public class UserDaoTest_Junit {
 		
 		User user1 = new User("spring1","spring1","springno1");
 		User user2 = new User("spring2","spring2","springno2");
-		User user3 = new User("spring3","spring3","springno3");
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 		
 		dao.add(user1);
-		
-		assertThat(dao.getCount(), is(1));
-		
 		dao.add(user2);
-		
-		assertThat(dao.getCount(), is(2));
 		 
-		dao.add(user3);
+		assertThat(dao.getCount(), is(2));
 		
-		assertThat(dao.getCount(), is(3));
-			
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(userget1.getName()));
+		assertThat(userget1.getPassword(), is(userget1.getPassword()));	
+		
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(userget2.getName()));
+		assertThat(userget2.getPassword(), is(userget2.getPassword()));	
 	}
 }
